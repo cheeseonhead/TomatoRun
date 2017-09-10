1d3d4b4f3c0c80cdda336931c23c41f1f95419c8
Jeff Woo <cheeseonhead@gmail.com>
2017-09-10 19:01:38 -0400

    Some cleanup

--- TomatoRun/TomatoRun/RunScene/RunViewController.swift
+++ TomatoRun/TomatoRun/RunScene/RunViewController.swift
@@ -14,27 +14,27 @@ class RunViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
         if let scene = GKScene(fileNamed: "RunScene") {
-            
+
             // Get the SKScene from the loaded GKScene
             if let sceneNode = scene.rootNode as! RunScene? {
-                
+
                 // Copy gameplay related content over to the scene
                 sceneNode.entities = scene.entities
                 sceneNode.graphs = scene.graphs
-                
+
                 // Set the scale mode to scale to fit the window
                 sceneNode.scaleMode = .aspectFill
-                
+
                 // Present the scene
                 if let view = self.view as! SKView? {
                     view.presentScene(sceneNode)
-                    
+
                     view.ignoresSiblingOrder = true
-                    
+
                     view.showsFPS = true
                     view.showsNodeCount = true
                 }
@@ -42,21 +42,8 @@ class RunViewController: UIViewController {
         }
     }
 
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+        return .portrait
     }
 
     override var prefersStatusBarHidden: Bool {
