864999f6906faf944672f02ebefc8d5b532190bb
Jeff Woo <cheeseonhead@gmail.com>
2017-09-10 19:09:37 -0400

    ImageConstants file

---
+++ TomatoRun/TomatoRun/Shared/Constants/ImageConstants.swift
@@ -0,0 +1,13 @@
+//
+//  ImageConstants.swift
+//  TomatoRun
+//
+//  Created by Cheese Onhead on 9/10/17.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import Foundation
+
+enum ImageConstants {
+    static let ropeImageName = "Rope"
+}
