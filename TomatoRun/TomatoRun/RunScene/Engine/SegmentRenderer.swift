288aa0a0d1e9d76022281454abbbdcec663990eb
Jeff Woo <cheeseonhead@gmail.com>
2017-09-10 23:35:31 -0400

    #4 | Added a segment renderer

---
+++ TomatoRun/TomatoRun/RunScene/Engine/SegmentRenderer.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentRenderer.swift
+//  TomatoRun
+//
+//  Created by Cheese Onhead on 9/10/17.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import SpriteKit
+
+class SegmentRenderer {
+    let scene: SKScene
+
+    init(scene: SKScene) {
+        self.scene = scene
+    }
+}
