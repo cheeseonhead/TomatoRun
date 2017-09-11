04f9c931ee2ca3913f555884ca263ad3a306bc8e
Jeff Woo <cheeseonhead@gmail.com>
2017-09-11 00:11:22 -0400

    #4 | Some renderer thingy

--- TomatoRun/TomatoRun/RunScene/Engine/SegmentRenderer.swift
+++ TomatoRun/TomatoRun/RunScene/Engine/SegmentRenderer.swift
@@ -8,15 +8,40 @@
 
 import SpriteKit
 
+private let renderBuffer = CGFloat(30)
+
 class SegmentRenderer {
     let segmentManager = SegmentManager()
-
     unowned let scene: RunScene
+
+    // MARK: State
+    var currentSegmentEnd = CGFloat(0)
 
     init(scene: RunScene) {
         self.scene = scene
     }
 
     func update(_: TimeInterval) {
+        guard let renderedFrame = scene.camera?.renderedFrame() else { return }
+
+        let maxY = renderedFrame.position(forType: .topRight).y
+        while currentSegmentEnd < maxY + renderBuffer {
+            if let newSegment = segmentManager.getNextSegment() {
+                addSegment(newSegment)
+            }
+        }
+    }
+
+    func addSegment(_ segment: Segment) {
+        var heights = [CGFloat]()
+        var rope = [Int]()
+
+        for board in segment.woodenBoards {
+            heights.append(currentSegmentEnd + board.height)
+            rope.append(board.rope)
+        }
+
+        scene.addBoards(atHeights: heights, ropeIndex: rope)
+        currentSegmentEnd += segment.length
     }
 }
