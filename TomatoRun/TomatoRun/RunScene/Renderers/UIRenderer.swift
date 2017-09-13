d58f185aeb4b6f344abe78b93cec58dade02fd63
Jeffrey Wu <cheeseonhead@gmail.com>
2017-09-13 13:41:05 -0400

    #18 | Adding a renderer

---
+++ TomatoRun/TomatoRun/RunScene/Renderers/UIRenderer.swift
@@ -0,0 +1,17 @@
+//
+//  UIRenderer.swift
+//  TomatoRun
+//
+//  Created by Jeffrey Wu on 2017-09-13.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import SpriteKit
+
+class UIRenderer {
+    unowned let scene: RunScene
+
+    init(scene: RunScene) {
+        self.scene = scene
+    }
+}
