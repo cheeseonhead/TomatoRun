65b13e98fb290e707e2d2e4728b0b6a3b5b7d5ae
Jeffrey Wu <cheeseonhead@gmail.com>
2017-09-13 14:10:01 -0400

    #18 | Added a pause node

---
+++ TomatoRun/TomatoRun/RunScene/Renderers/Nodes/PauseNode.swift
@@ -0,0 +1,20 @@
+//
+//  PauseNode.swift
+//  TomatoRun
+//
+//  Created by Jeffrey Wu on 2017-09-13.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import SpriteKit
+
+class PauseNode: SKSpriteNode {
+    init(size: CGSize) {
+        super.init(texture: nil, color: .red, size: size)
+        zPosition = RunSceneConstants.ZPositions.UIZPosition
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
