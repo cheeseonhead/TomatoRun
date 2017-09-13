df5a9027748879c284252b98d36772b73af5af4a
Jeffrey Wu <cheeseonhead@gmail.com>
2017-09-13 12:00:40 -0400

    #14 | Initially add two new files

---
+++ TomatoRun/TomatoRun/RunScene/Entities/SpiderEntity.swift
@@ -0,0 +1,9 @@
+//
+//  SpiderEntity.swift
+//  TomatoRun
+//
+//  Created by Jeffrey Wu on 2017-09-13.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import Foundation
---
+++ TomatoRun/TomatoRun/Shared/GKEntity/GKEntity+Positional.swift
@@ -0,0 +1,9 @@
+//
+//  GKEntity+Positional.swift
+//  TomatoRun
+//
+//  Created by Jeffrey Wu on 2017-09-13.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import Foundation
