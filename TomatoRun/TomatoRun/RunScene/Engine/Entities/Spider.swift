ca107cbd5c026c7cc747481f3d3abcfa4659f9de
Jeffrey Wu <cheeseonhead@gmail.com>
2017-09-13 12:14:09 -0400

    #14 | Added spider struct

---
+++ TomatoRun/TomatoRun/RunScene/Engine/Entities/Spider.swift
@@ -0,0 +1,14 @@
+//
+//  Spider.swift
+//  TomatoRun
+//
+//  Created by Jeffrey Wu on 2017-09-13.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import Foundation
+
+struct Spider: Codable {
+    var height: CGFloat
+    var rope: Int
+}
