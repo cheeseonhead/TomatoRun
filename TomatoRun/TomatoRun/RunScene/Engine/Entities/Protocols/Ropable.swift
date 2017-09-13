9e7f7d600ae597e218a0140bd72d51c0d2d1fc38
Jeffrey Wu <cheeseonhead@gmail.com>
2017-09-13 12:49:43 -0400

    #14 | Add new protocol

---
+++ TomatoRun/TomatoRun/RunScene/Engine/Entities/Protocols/Ropable.swift
@@ -0,0 +1,14 @@
+//
+//  Ropable.swift
+//  TomatoRun
+//
+//  Created by Jeffrey Wu on 2017-09-13.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import UIKit
+
+protocol Ropable {
+    let height: CGFloat { get set }
+    let rope: Int { get set }
+}
