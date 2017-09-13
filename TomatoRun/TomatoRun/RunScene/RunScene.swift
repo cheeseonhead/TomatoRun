1425b8e1670750f867d24ba8d4941d45214a6aca
Jeffrey Wu <cheeseonhead@gmail.com>
2017-09-13 13:00:50 -0400

    #14 | Made the spider smaller

--- TomatoRun/TomatoRun/RunScene/RunScene.swift
+++ TomatoRun/TomatoRun/RunScene/RunScene.swift
@@ -86,7 +86,7 @@ extension RunScene {
         guard heights.count == ropeIndex.count else { return }
 
         for i in 0 ..< heights.count {
-            let spider = SpiderEntity(fittingWidth: size.width * 0.2)
+            let spider = SpiderEntity(fittingWidth: size.width * 0.1)
 
             addEntity(spider, toRope: ropeIndex[i], atHeight: heights[i])
         }
