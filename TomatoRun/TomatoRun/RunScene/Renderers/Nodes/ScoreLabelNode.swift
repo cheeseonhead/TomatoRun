5cce98c334788970232a751945c21a207c2b6f71
Jeff Woo <cheeseonhead@gmail.com>
2017-09-13 20:14:40 -0400

    #18 | Made score label better

--- TomatoRun/TomatoRun/RunScene/Renderers/Nodes/ScoreLabelNode.swift
+++ TomatoRun/TomatoRun/RunScene/Renderers/Nodes/ScoreLabelNode.swift
@@ -9,11 +9,34 @@
 import SpriteKit
 
 class ScoreLabelNode: SKLabelNode {
+    var score: Int? {
+        didSet {
+            guard let score = score else { return }
+
+            text = formattedScore(score)
+        }
+    }
+
     override init() {
-        super.init(fontNamed: "Helvetica")
+        super.init()
+
+        verticalAlignmentMode = .baseline
+        horizontalAlignmentMode = .left
+
+        zPosition = RunSceneConstants.ZPositions.UIZPosition
+    }
+
+    convenience init(score: Int?) {
+        self.init()
+
+        self.score = score
     }
 
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    func formattedScore(_ score: Int) -> String {
+        return String(format: "%010d", score)
     }
 }
