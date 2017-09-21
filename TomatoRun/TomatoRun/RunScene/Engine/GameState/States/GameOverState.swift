ea6a58711d3cc23ba2e7be612074701670c42a99
Jeffrey Wu <cheeseonhead@gmail.com>
2017-09-20 22:31:03 -0400

    #17 | Game over state

---
+++ TomatoRun/TomatoRun/RunScene/Engine/GameState/States/GameOverState.swift
@@ -0,0 +1,12 @@
+//
+//  GameOverState.swift
+//  TomatoRun
+//
+//  Created by Jeffrey Wu on 2017-09-20.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import GameplayKit
+
+class GameOverState: GKState {
+}
