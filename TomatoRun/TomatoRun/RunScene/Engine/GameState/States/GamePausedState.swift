3cc94d34464b9cbbc56113eefcda779fa6f2a3fc
Jeffrey Wu <cheeseonhead@gmail.com>
2017-09-20 22:27:55 -0400

    #17 | Some more states

---
+++ TomatoRun/TomatoRun/RunScene/Engine/GameState/States/GamePausedState.swift
@@ -0,0 +1,12 @@
+//
+//  GamePausedState.swift
+//  TomatoRun
+//
+//  Created by Jeffrey Wu on 2017-09-20.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import GameplayKit
+
+class GamePausedState: GKState {
+}
