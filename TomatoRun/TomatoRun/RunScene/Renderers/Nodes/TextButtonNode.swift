7d5d107112a508db383f6590cc6e36e75d2a68bf
Jeff Woo <cheeseonhead@gmail.com>
2017-09-23 19:47:18 -0400

    #28 | Implemented a little bit of this

--- TomatoRun/TomatoRun/RunScene/Renderers/Nodes/TextButtonNode.swift
+++ TomatoRun/TomatoRun/RunScene/Renderers/Nodes/TextButtonNode.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2017 okAy Studios. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+class TextButtonNode: SKNode {
+
+    let label: SKLabelNode
+    let backgroundNode: SKSpriteNode
+
+    init(text: Stringk, size: CGSize) {
+        super.init()
+
+        label = SKLabelNode(text: text)
+        label.zPosition = 500
+        backgroundNode = SKSpriteNode(texture: SKTexture(imageNamed: "WoodenBoard"), color: .red, size: size)
+        label.zPosition = 0
+
+        addChildren([label, backgroundNode])
+    }
+
+    override init() {
+        self.init(text: "")
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
