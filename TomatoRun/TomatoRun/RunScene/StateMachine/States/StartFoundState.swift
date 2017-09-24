75b5e5a06118d8c273f5905c6678cf81673b9b4b
Jeff Woo <cheeseonhead@gmail.com>
2017-09-24 00:18:47 -0400

    #30 | Simple

--- TomatoRun/TomatoRun/RunScene/StateMachine/States/StartFoundState.swift
+++ TomatoRun/TomatoRun/RunScene/StateMachine/States/StartFoundState.swift
@@ -6,4 +6,12 @@
 //  Copyright © 2017 okAy Studios. All rights reserved.
 //
 
-import Foundation
+import GameplayKit
+
+class StartFoundState: GKState {
+    var target: Target?
+
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
+        return stateClass == TravelingToStartState.self
+    }
+}
