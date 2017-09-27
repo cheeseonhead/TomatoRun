e11ddb66fe376a3c6b2bae9e06ef097756c87b4b
Jeff Woo <cheeseonhead@gmail.com>
2017-09-27 06:54:48 -0400

    #35 | Added a fpfunction class

---
+++ TomatoRun/TomatoRun/Shared/FPFunctions.swift
@@ -0,0 +1,23 @@
+//
+//  FPFunctions.swift
+//  TomatoRun
+//
+//  Created by Cheese Onhead on 9/27/17.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import Foundation
+
+func filterUnion<T>(_ filter1: @escaping (T) -> Bool, _ filter2: @escaping (T) -> Bool) -> (T) -> Bool {
+    return { item in
+        filter1(item) && filter2(item)
+    }
+}
+
+func chooseBest<T>(from set: Set<T>, compare: (T, T) -> T) -> T? {
+    guard let first = set.first else { return nil }
+
+    return set.reduce(first, { (curBest, curItem) -> T in
+        compare(curBest, curItem)
+    })
+}
