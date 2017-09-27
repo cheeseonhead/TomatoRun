c8069415b26139e26b245b97e296ae7a19c192f4
Sydna <andysheng95@hotmail.com>
2017-09-27 17:06:47 -0400

    #38 | Added the sidebar entity

--- TomatoRun/TomatoRun/RunScene/Entities/SidebarEntity.swift
+++ TomatoRun/TomatoRun/RunScene/Entities/SidebarEntity.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2017 okAy Studios. All rights reserved.
 //
 
-import Foundation
+import GameplayKit
+import SpriteKit
+
+class SidebarEntity: GKEntity {
+    init(position: CGPoint, scene: SKScene) {
+        super.init()
+
+        let texture = SKTexture(imageNamed: ImageConstants.SidebarImageName)
+        let continuousSprite = ContinuousSpriteComponent(scene: scene, texture: texture, position: position, zPosition: RunSceneConstants.ZPositions.Sidebar)
+
+        addComponent(continuousSprite)
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
