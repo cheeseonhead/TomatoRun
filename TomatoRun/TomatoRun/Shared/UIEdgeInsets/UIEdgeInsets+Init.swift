060166c4cd2549de61b451ccf27fbd4e708264c4
SydnaA <andy.sheng@mail.utoronto.ca>
2017-09-27 13:46:26 -0400

    #40 | Improved Main Menu scene (#41)

---
+++ TomatoRun/TomatoRun/Shared/UIEdgeInsets/UIEdgeInsets+Init.swift
@@ -0,0 +1,15 @@
+//
+//  UIEdgeInsets+Init.swift
+//  TomatoRun
+//
+//  Created by Andy Sheng on 2017-09-27.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import UIKit
+
+extension UIEdgeInsets {
+    public init(inset: CGFloat) {
+        self.init(top: inset, left: inset, bottom: inset, right: inset)
+    }
+}
