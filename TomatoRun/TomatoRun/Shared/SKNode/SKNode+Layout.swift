0320e36aaff3f3651f980b4981c4da30d32de464
Jeff Woo <cheeseonhead@gmail.com>
2017-10-07 18:33:07 -0400

    #55 | Added a bunch of layout things

---
+++ TomatoRun/TomatoRun/Shared/SKNode/SKNode+Layout.swift
@@ -0,0 +1,56 @@
+//
+//  SKNode+Layout.swift
+//  TomatoRun
+//
+//  Created by Cheese Onhead on 10/7/17.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+typealias Layout = (SKNode, SKNode) -> CGPoint
+
+func ==>(layout: @escaping Layout, f: (@escaping Layout) -> Layout) -> Layout {
+    return f(layout)
+}
+
+extension SKNode {
+
+    func height() -> CGFloat {
+        return frame.size.height
+    }
+
+    static func none() -> Layout {
+        return { _, node in
+            node.position
+        }
+    }
+}
+
+let belowCentered: Layout = SKNode.none() ==> below ==> centerHorizontally
+
+func offset(_ offset: CGPoint) -> (@escaping Layout) -> Layout {
+    return { layout in { anchor, node in
+        let position = layout(anchor, node)
+        return position + offset
+    }
+    }
+}
+
+func below(_ layout: @escaping Layout) -> Layout {
+    return { anchor, node in
+        let position = layout(anchor, node)
+        let y = anchor.position.y - anchor.height() / 2 - node.height() / 2
+
+        return CGPoint(x: position.x, y: y)
+    }
+}
+
+func centerHorizontally(_ layout: @escaping Layout) -> Layout {
+    return { anchor, node in
+        let position = layout(anchor, node)
+
+        return CGPoint(x: anchor.position.x, y: position.y)
+    }
+}
