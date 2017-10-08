f4254cc53230bab8487c2f3131bfc2736dfe61ad
Sydna <andysheng95@hotmail.com>
2017-10-08 01:24:31 -0400

    #49 | Added TextLabelComponent

---
+++ TomatoRun/TomatoRun/RunScene/Components/TextLabelComponent.swift
@@ -0,0 +1,44 @@
+//
+//  TextLabelComponent.swift
+//  TomatoRun
+//
+//  Created by Andy Sheng on 2017-09-29.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class TextLavelComponent: GKComponent {
+    let node: SKLabelNode
+
+    init(text: String) {
+        node = SKLabelNode(text: text)
+        super.init()
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func didAddToEntity() {
+        super.didAddToEntity()
+        node.entity = entity
+    }
+}
+
+extension SpriteComponent: Positionable {
+    func setPosition(_ position: CGPoint) {
+        node.position = position
+    }
+
+    func getPosition() -> CGPoint {
+        return node.position
+    }
+}
+
+extension SpriteComponent: Framable {
+    func frame() -> CGRect {
+        return node.frame
+    }
+}
