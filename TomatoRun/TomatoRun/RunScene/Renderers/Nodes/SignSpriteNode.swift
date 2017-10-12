e3793db6a97eff2876b688504332aca49e63d637
Jeff Woo <cheeseonhead@gmail.com>
2017-10-12 11:10:32 -0400

    #49 | Added class for sign sprite node

---
+++ TomatoRun/TomatoRun/RunScene/Renderers/Nodes/SignSpriteNode.swift
@@ -0,0 +1,12 @@
+//
+//  SignSpriteNode.swift
+//  TomatoRun
+//
+//  Created by Cheese Onhead on 10/12/17.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import SpriteKit
+
+class SignSpriteNode: SKSpriteNode {
+}
