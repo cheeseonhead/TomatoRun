f7c1c79fcfc0fa04b6cc1937056210ed17ad6fdc
Jeff Woo <cheeseonhead@gmail.com>
2017-10-12 12:24:44 -0400

    #49 | Added some code to sign sprite node

--- TomatoRun/TomatoRun/RunScene/Renderers/Nodes/SignSpriteNode.swift
+++ TomatoRun/TomatoRun/RunScene/Renderers/Nodes/SignSpriteNode.swift
@@ -9,4 +9,20 @@
 import SpriteKit
 
 class SignSpriteNode: SKSpriteNode {
+
+    override init(texture _: SKTexture?, color: UIColor, size: CGSize) {
+        super.init(texture: nil, color: color, size: size)
+
+        let signBody = SKShapeNode(rectOf: size)
+        signBody.fillColor = color
+        addChild(signBody)
+    }
+
+    convenience init(color: UIColor, size: CGSize) {
+        self.init(texture: nil, color: color, size: size)
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
