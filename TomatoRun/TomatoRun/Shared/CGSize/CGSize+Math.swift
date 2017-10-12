0c838b07d98098c94f458cd92ec00d800d19ac92
Jeff Woo <cheeseonhead@gmail.com>
2017-10-12 12:10:16 -0400

    #49 | More math

--- TomatoRun/TomatoRun/Shared/CGSize/CGSize+Math.swift
+++ TomatoRun/TomatoRun/Shared/CGSize/CGSize+Math.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
+func pointWise(_ f: (CGFloat, CGFloat) -> CGFloat, l: CGSize, r: CGSize) -> CGSize {
+    return CGSize(width: f(l.width, r.width), height: f(l.height, r.height))
+}
+
 func -(l: CGSize, r: CGSize) -> CGSize {
-    return CGSize(width: l.width - r.width, height: l.height - r.height)
+    return pointWise(-, l: l, r: r)
 }
 
 func *(scale: CGFloat, r: CGSize) -> CGSize {
@@ -17,9 +21,34 @@ func *(scale: CGFloat, r: CGSize) -> CGSize {
 }
 
 func *(l: CGSize, r: CGSize) -> CGSize {
-    return CGSize(width: l.width * r.width, height: l.height * r.height)
+    return pointWise(*, l: l, r: r)
 }
 
 func /(l: CGSize, r: CGSize) -> CGSize {
-    return CGSize(width: l.width / r.width, height: l.height / r.height)
+    return pointWise(/, l: l, r: r)
+}
+
+func isHorizontalEdge(_ edge: CGRectEdge) -> Bool {
+    switch edge {
+    case .maxXEdge, .minXEdge:
+        return true
+    default:
+        return false
+    }
+}
+
+func splitRect(rect: CGRect, sizeRatio: CGSize, edge: CGRectEdge) -> (CGRect, CGRect) {
+    let ratio = isHorizontalEdge(edge) ? sizeRatio.width : sizeRatio.height
+    let multiplier = isHorizontalEdge(edge) ? rect.width : rect.height
+    let distance = multiplier * ratio
+
+    return rect.divided(atDistance: distance, from: edge)
+}
+
+func splitHorizontal(_ rect: CGRect, ratio: CGSize) -> (CGRect, CGRect) {
+    return splitRect(rect: rect, sizeRatio: ratio, edge: .minXEdge)
+}
+
+func splitVertical(_ rect: CGRect, ratio: CGSize) -> (CGRect, CGRect) {
+    return splitRect(rect: rect, sizeRatio: ratio, edge: .minYEdge)
 }
