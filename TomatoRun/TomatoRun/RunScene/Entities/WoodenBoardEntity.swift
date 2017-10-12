dfdac01dbb7da3b4613093e1c0c870be7dd5591b
Jeff Woo <cheeseonhead@gmail.com>
2017-10-11 21:05:11 -0400

    #49 | Made it required

--- TomatoRun/TomatoRun/RunScene/Entities/WoodenBoardEntity.swift
+++ TomatoRun/TomatoRun/RunScene/Entities/WoodenBoardEntity.swift
@@ -9,7 +9,7 @@ import SpriteKit
 class WoodenBoardEntity: GKEntity, Positionable {
     let intersectionAnchors = [CGPoint(x: 0, y: 0.5), CGPoint(x: 1, y: 0.5)]
 
-    init(fittingWidth: CGFloat, entityManager: EntityManager, textEnabled: Bool = true) {
+    init(fittingWidth: CGFloat, entityManager: EntityManager, textEnabled: Bool) {
         super.init()
 
         let texture = SKTexture(imageNamed: ImageConstants.WoodenBoardImageName)
