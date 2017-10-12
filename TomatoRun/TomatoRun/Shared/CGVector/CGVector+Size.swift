2218ab8bcac9adc892c0d67d94db886a837e8039
Jeff Woo <cheeseonhead@gmail.com>
2017-10-12 11:38:30 -0400

    #49 | Added a size thing to vector

---
+++ TomatoRun/TomatoRun/Shared/CGVector/CGVector+Size.swift
@@ -0,0 +1,15 @@
+//
+//  CGVector+Size.swift
+//  TomatoRun
+//
+//  Created by Cheese Onhead on 10/12/17.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import Foundation
+
+extension CGVector {
+    func size() -> CGSize {
+        return CGSize(width: x, height: y)
+    }
+}
