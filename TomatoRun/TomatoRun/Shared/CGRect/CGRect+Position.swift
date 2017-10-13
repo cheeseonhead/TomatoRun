7110fd358a905ded9964fb810c9cac8f364205c5
Jeff Woo <cheeseonhead@gmail.com>
2017-10-12 23:48:15 -0400

    #49 | Added two composite functions

--- TomatoRun/TomatoRun/Shared/CGRect/CGRect+Position.swift
+++ TomatoRun/TomatoRun/Shared/CGRect/CGRect+Position.swift
@@ -8,8 +8,33 @@
 
 import UIKit
 
+typealias Layout = (CGRect) -> CGRect
+
 extension CGRect {
-    static func positioned(anchor: CGPoint, at point: CGPoint) -> (CGRect) -> CGRect {
+
+    // MARK: - Composite
+
+    static func offsetted(by vector: CGVector) -> (@escaping Layout) -> Layout {
+        return { layout in { frame in
+            let result = layout(frame)
+            let newOrigin = result.origin + vector
+
+            return CGRect(origin: newOrigin, size: frame.size)
+        }
+        }
+    }
+
+    static func above(frame anchor: CGRect) -> (@escaping Layout) -> Layout {
+        return { layout in { frame in
+            let result = layout(frame)
+            return result.positioned(edge: .minYEdge, at: anchor.position(forEdge: .maxYEdge))
+        }
+        }
+    }
+
+    // MARK: - Pure
+
+    static func positioned(anchor: CGPoint, at point: CGPoint) -> Layout {
         return { frame in
             let offset = point - frame.position(forAnchor: anchor)
             let newOrigin = frame.origin + offset
@@ -18,7 +43,7 @@ extension CGRect {
         }
     }
 
-    static func positioned(anchorType type: PointType, at point: CGPoint) -> (CGRect) -> CGRect {
+    static func positioned(anchorType type: PointType, at point: CGPoint) -> Layout {
 
         return { frame in
             let anchorPoint = CGRect.anchor(forType: type)
@@ -26,4 +51,10 @@ extension CGRect {
             return positioned(anchor: anchorPoint, at: point)(frame)
         }
     }
+}
+
+infix operator >==: AdditionPrecedence
+// MARK: - Math
+func >==(layout: Layout, f: @escaping (Layout) -> Layout) -> Layout {
+    return f(layout)
 }
