02a397e76bf6ac001cb4360d2a6801b0fbc6bad6
Jeff Woo <cheeseonhead@gmail.com>
2017-12-20 20:21:45 -0500

    #56 | Setup revive scene

--- TomatoRun/TomatoRun/RunScene/Scenes/ReviveScene/ReviveScene.swift
+++ TomatoRun/TomatoRun/RunScene/Scenes/ReviveScene/ReviveScene.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2017 okAy Studios. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+class ReviveScene: SKScene, RunPresentable {
+
+    var runViewController: RunViewController?
+    var gameStateMachine: GameStateMachine!
+
+    var scoreLabel: ScoreLabelNode!
+    var watchVideoButton: TextButtonNode!
+    var cancelButton: TextButtonNode!
+
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+
+        scoreLabel = ScoreLabelNode(score: 0)
+        gameStateMachine?.getFinalScore { score in
+            scoreLabel.score = score
+        }
+
+        watchVideoButton = TextButtonNode(text: "WATCH VID", fontSize: .Large)
+
+        layoutNodes()
+
+        addChildren([scoreLabel, watchVideoButton])
+    }
+}
+
+private extension ReviveScene {
+    func layoutNodes() {
+        scoreLabel.layoutUsing(CGRect.positioned(anchorType: .center, at: CGPoint.zero))
+        let buttonLayout = CGRect.belowCentered(scoreLabel.frame) >== CGRect.offsetted(by: CGVector(dx: 0, dy: -60))
+        watchVideoButton.layoutUsing(buttonLayout)
+    }
+}
